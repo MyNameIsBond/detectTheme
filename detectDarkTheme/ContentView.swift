0cea9e769671e558f07820ce4560da645a66b469
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-12-06 04:23:40 +0000

    almost finished

--- detectDarkTheme/ContentView.swift
+++ detectDarkTheme/ContentView.swift
@@ -1,11 +1,31 @@
-//
-//  ContentView.swift
-//  detectDarkTheme
-//
-//  Created by Tony Hajdini on 05/12/2020.
-//
 
 import SwiftUI
+
+struct Card: Identifiable {
+    let id = UUID()
+    let title: String
+    let subTitle: String
+    let icon: String
+}
+
+
+
+extension Color {
+    
+    //light theme
+    static let lightBackground = Color(red: 224 / 255, green: 229 / 255, blue: 236 / 255)
+    static let lightShadow = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255) // opacity 0.5
+    static let darkShadow = Color(red: 163 / 255, green: 177 / 255, blue: 198 / 255)
+    
+    //dark theme
+    static let darkBackground = Color(red: 78 / 255, green: 78 / 255, blue: 80 / 255)
+}
+
+var cards = [
+    Card(title: "Title", subTitle: "Subtitle", icon: "heart")
+]
+
+
 
 struct ContentView: View {
     var body: some View {
